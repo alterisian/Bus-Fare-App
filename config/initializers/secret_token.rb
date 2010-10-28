5f766d2f5913c33f4a9abf0716c6f97db70e88da
Ian Moss <ian@liquidbronze.com>
2010-10-28 13:29:36 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Busfare::Application.config.secret_token = 'be051581e7698d5b4bd8b9e61cf046b2f352a3e29d098c94590d93c3a0bab1de445044dbb4266ea6130b8e43c0b00712752701867939db3c778d3ee4478fe564'
